1c05762bd2150c493e5f52294c8ede91a714a64c
Cari Lewis <70178270+Cari-D-Lewis@users.noreply.github.com>
2022-11-18 15:28:53 -0600

    Add files via upload

--- Assignments/GIS_Lesson/GIS_R_20-Oct-22.R
+++ Assignments/GIS_Lesson/GIS_R_20-Oct-22.R
@@ -80,3 +80,100 @@ my_sites_shape
 plot(my_clim_stack[[2]])
 plot(countries, add=TRUE)
 points(my_sites_shape, pch=16) # show sites on the map
+
+# check the plot projections
+projection(my_sites_shape)
+projection(countries)
+
+# add a column to countries data frame with a unique number
+countries$id <- 1:nrow(countries)
+
+# see which countries points fall into
+my_countries <- over(my_sites_shape, countries)
+head(as.data.frame(my_countries)) # let's look at data frame portion
+
+my_countries <- countries[countries$id %in% my_countries$id, ] #select my countries from the country shape file by id #
+# now let's plot just my countries
+plot(my_countries)
+points(my_sites_shape, col='red', pch=16)
+
+# convert shapefile to raster
+my_countries_mask <- rasterize(my_countries, my_clim_stack[[2]])
+my_countries_mask <- my_countries_mask * 0 + 1 # make all values 1
+
+my_clim_sites <- my_clim_stack[[2]] * my_countries_mask
+plot(my_clim_sites)
+
+bg <- as.data.frame(randomPoints(my_clim_stack, n=10000)) # 10,000 random sites
+names(bg) <- c('longitude', 'latitude')
+head(bg)
+
+plot(my_clim_stack[[1]])
+points(bg, pch='.') # plot on map
+
+# extract enviro variables for the random points
+bgEnv <- as.data.frame(extract(my_clim_stack, bg))
+head(bgEnv)
+
+bg <- cbind(bg, bgEnv)
+head(bg)
+
+pres_bg <- c(rep(1, nrow(my_sites)), rep(0, nrow(bg)))
+
+train_data <- data.frame(
+  pres_bg=pres_bg,
+  rbind(my_sites, bg)
+)
+
+head(train_data)
+
+my_model <- glm(
+  pres_bg ~ mean_diurnal_range*temperature_seasonality*precip_driest_quarter + I(mean_diurnal_range^2) + I(temperature_seasonality^2) + I(precip_driest_quarter^2),
+  data=train_data,
+  family='binomial',
+  weights=c(rep(1, nrow(my_sites)), rep(nrow(my_sites) / nrow(bg), nrow(bg)))
+)
+
+summary(my_model)
+
+my_world <- predict(
+  my_clim_stack,
+  my_model,
+  type='response'
+)
+
+my_world
+
+plot(my_world)
+plot(countries, add=TRUE)
+points(my_sites_shape, col='red', pch=16)
+
+writeRaster(my_world, 'My_Climate_Niche/my_world', format='GTiff', overwrite=TRUE, progress='text')
+
+# threshold your "preferred" climate
+my_world_thresh <- my_world >= quantile(my_world, 0.75)
+plot(my_world_thresh)
+
+# convert all values not equal to 1 to NA...
+# using "calc" function to implement a custom function
+my_world_thresh <- calc(my_world_thresh, fun=function(x) ifelse(x==0 | is.na(x), NA, 1))
+
+# get random sites
+my_best_sites <- randomPoints(my_world_thresh, 10000)
+my_best_env <- as.data.frame(extract(my_clim_stack, my_best_sites))
+
+# plot world's climate
+smoothScatter(x=bgEnv$temperature_seasonality, y=bgEnv$precip_driest_quarter, col='lightblue')
+points(my_best_env$temperature_seasonality, my_best_env$precip_driest_quarter, col='red', pch=16, cex=0.2)
+points(my_sites$temperature_seasonality, my_sites$precip_driest_quarter, pch=16)
+legend(
+  'bottomright',
+  inset=0.01,
+  legend=c('world', 'my niche', 'my locations'),
+  pch=16,
+  col=c('lightblue', 'red', 'black'),
+  pt.cex=c(1, 0.4, 1)
+  
+)
+
+## NOTES PULLED FROM THE COURSE LESSON ONLINE WRITTEN BY DR. TOOMEY
